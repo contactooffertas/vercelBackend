6fbc382249690e9a2d5aa1ccfcc82730b545df4d
contactooffertas <contacto.offertas@gmail.com>
2026-03-01 00:48:29 -0300

    Update sendMail.js

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,18 +1,14 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass:process.env.EMAIL_PASS,
+    user: "contacto.offertas@gmail.com",
+    pass: "dikd rskl kdcq qkpy",
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-});
-
+});;
 
 const sendEmail = async (to, subject, text, html) => {
   try {
@@ -31,6 +27,7 @@ const sendEmail = async (to, subject, text, html) => {
 
 
 module.exports = sendEmail;
+
 
 
 
