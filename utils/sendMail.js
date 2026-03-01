a42b440a747466f0dcd32b1a53b4afc4ea217bcf
contactooffertas <contacto.offertas@gmail.com>
2026-03-01 16:24:28 -0300

    Update sendMail.js

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,34 +1,29 @@
-const nodemailer = require("nodemailer");
+// utils/sendMail.js
+const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
+  host:   'smtp.gmail.com',
+  port:   465,
   secure: true,
   auth: {
     user: "contacto.offertas@gmail.com",
     pass: "dikd rskl kdcq qkpy",
   },
   connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  greetingTimeout:   10000,
+  socketTimeout:     10000,
 });
 
 const sendEmail = async (to, subject, text, html) => {
-  try {
-    await transporter.sendMail({
-      from: `"Offertas"- contacto.offertas@gmail.com`,
-      to,
-      subject,
-      text,
-      html: html || text,
-    });
-  } catch (err) {
-    console.error("❌ Error enviando email:", err);
-    throw err;
-  }
+  await transporter.sendMail({
+    from:    `"Offerton" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    text,
+    html: html || text,
+  });
 };
-
-  console.log('✅ Este es del sendMail');
 
 module.exports = sendEmail;
 
+  
