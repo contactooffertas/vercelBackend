8a8996016fa397b5ac83e29667299312351066da
contactooffertas <contacto.offertas@gmail.com>
2026-03-01 14:33:21 -0300

    Update sendMail.js

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -8,23 +8,26 @@ const transporter = nodemailer.createTransport({
     user: "contacto.offertas@gmail.com",
     pass: "dikd rskl kdcq qkpy",
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 const sendEmail = async (to, subject, text, html) => {
   try {
-    const info = await transporter.sendMail({
-      from: '"Offertas" <contacto.offertas@gmail.com>',
-      to: Array.isArray(to) ? to.join(",") : to,
+    await transporter.sendMail({
+      from: `"Offertas"- ${process.env.EMAIL_USER}`,
+      to,
       subject,
       text,
       html: html || text,
     });
-    console.log("✅ Email enviado:", info.messageId);
-    return info;
   } catch (err) {
     console.error("❌ Error enviando email:", err);
     throw err;
   }
 };
+
+  console.log('✅ Este es del sendMail');
 
 module.exports = sendEmail;
