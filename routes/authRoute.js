df73ee2c0b04a9ffff36d2de88fc6dd75db870a7
contactooffertas <contacto.offertas@gmail.com>
2026-03-01 16:26:32 -0300

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,19 +1,23 @@
 // routes/authRoute.js
-const express = require('express');
-const router = express.Router();
+const express        = require('express');
+const router         = express.Router();
+const login          = require('../authController/login');
+const {
+  register,
+  verifyUser,
+  resendVerificationCode,
+  forgotPassword,
+  resetPassword,
+} = require('../authController/register');
+const authMiddleware  = require('../middleware/authMiddleware');
+const userController  = require('../authController/userController');
 
-const login = require('../authController/login');
-const { register, verifyUser, resendVerificationCode,forgotPassword,resetPassword } = require('../authController/register');
-const authMiddleware = require('../middleware/authMiddleware');
-const userController = require('../authController/userController');
-
-router.post('/register', register);
-router.post('/resend',   resendVerificationCode);
-router.post('/verify',   verifyUser);
-router.post('/login',    login);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password',  resetPassword);
-// Reemplaza actualizar.js directamente con userController.updateProfile
+router.post('/register',         register);
+router.post('/resend',           resendVerificationCode);
+router.post('/verify',           verifyUser);
+router.post('/login',            login);
+router.post('/forgot-password',  forgotPassword);
+router.post('/reset-password',   resetPassword);
 router.put('/update', authMiddleware, userController.updateProfile);
 
 module.exports = router;
