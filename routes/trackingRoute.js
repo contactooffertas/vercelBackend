70f63f71094455d3c76bdeb431376c3c6d051eff
contactooffertas <contacto.offertas@gmail.com>
2026-07-31 18:07:19 -0300

    Update trackingRoute.js

--- routes/trackingRoute.js
+++ routes/trackingRoute.js
@@ -6,7 +6,6 @@ const controller = require('../controllers/trackingController');
 // Público: cualquier web puede trackear (después le pones rate-limit)
 router.post('/event', controller.trackEvent);
 
-
 // Privado: solo el dueño del negocio
 const { protect } = require('../middlewares/authMiddleware'); // tu middleware
 router.get('/leads/:businessId', protect, controller.getLeadsByBusiness);
