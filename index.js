359577ea7010c7dd7ddcc39251bc6b2fb7cbdea8
contactooffertas <contacto.offertas@gmail.com>
2026-07-31 17:31:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors      = require('cors');
 const path      = require('path');
 const connectDB = require('./config/db');
 
+
 // ── Rutas ─────────────────────────────────────────────────────────────────────
 const authRoutes           = require('./routes/authRoute');
 const userRoutes           = require('./routes/userRoute');
