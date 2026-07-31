9aad775255454a595e287b55324b550d31650cb8
contactooffertas <contacto.offertas@gmail.com>
2026-07-31 18:37:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const announcementRoutes   = require('./routes/announcementRoute');
 const terminosRoutes       = require('./routes/terminosRoute');
 const eliminaUsuarioRoutes = require('./routes/eliminarUsuarioRoute');
 const shareRoutes          = require('./routes/shareRoute'); 
+const trackingRoutes = require('./routes/trackingRoutes');
 
 const { router: pushRoutes } = require('./routes/pushRoute');
 // ─────────────────────────────────────────────────────────────────────────────
