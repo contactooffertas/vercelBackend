92a13ba9657c3df1a01ac3185ae7b56e131a789c
contactooffertas <contacto.offertas@gmail.com>
2026-08-29 19:30:17 -0300

    Update sendMail.js

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
   secure: true,
   auth: {
     user: "contacto.offertas@gmail.com",
-    pass: "dikd rskl kdcq qkpy",
+    pass: "mure nbuc fqbh iwry",
   },
   connectionTimeout: 10000,
   greetingTimeout:   10000,
