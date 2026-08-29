f9dd5bbf6c5d3182b74d50253f5f25df759cd989
contactooffertas <contacto.offertas@gmail.com>
2026-08-29 19:41:06 -0300

    Update eliminaUsuarioController.js

--- authController/eliminaUsuarioController.js
+++ authController/eliminaUsuarioController.js
@@ -15,8 +15,8 @@ function createTransporter() {
     port:   parseInt(process.env.SMTP_PORT || "587"),
     secure: process.env.SMTP_SECURE === "true",
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+       user: "contacto.offertas@gmail.com",
+       pass: "mure nbuc fqbh iwry",
     },
   });
 }
